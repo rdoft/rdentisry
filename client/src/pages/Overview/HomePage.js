5c5a143f8d1e7138ffe316ce2f559d2db796371a
tnhnatalay <tnhnatalay@gmail.com>
2023-02-19 23:04:35 +0300

    🎮 Feat in homePage button icons RDEN-7
    Added icons
    Added  new cards
    Changed Application Name
    Changed icons and text space

--- client/src/pages/Overview/HomePage.js
+++ client/src/pages/Overview/HomePage.js
@@ -12,6 +12,12 @@ import Feature from "components/cards/Feature";
 
 // assets
 import AppointmentIcon from "assets/images/icons/appointment.png";
+import PatientsIcon from "assets/images/icons/patients.png";
+import CalenderIcon from "assets/images/icons/calender.png";
+import ToDoIcon from "assets/images/icons/todo.png";
+import DentistIcon from "assets/images/icons/dentist.png";
+import BarChartIcon from "assets/images/icons/barchart.png";
+import PaymentIcon from "assets/images/icons/wallet.png";
 
 const HomePage = () => {
   return (
@@ -20,8 +26,28 @@ const HomePage = () => {
         <Typography variant="h5">Anasayfa</Typography>
       </Grid>
 
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <Feature image={AppointmentIcon} title="Upcomming Appointment" />
+      <Grid item xs={6} sm={4} md={3} lg={2}>
+        <Feature image={PatientsIcon} title="Hastalar" />
+      </Grid>
+
+      <Grid item xs={6} sm={4} md={3} lg={2}>
+        <Feature image={DentistIcon} title="Doktorlar" />
+      </Grid>
+      
+      <Grid item xs={6} sm={4} md={3} lg={2}>
+        <Feature image={CalenderIcon} title="Takvim" />
+      </Grid>
+
+      <Grid item xs={6} sm={4} md={3} lg={2}>
+        <Feature image={ToDoIcon} title="İşlemler" />
+      </Grid>
+
+      <Grid item xs={6} sm={4} md={3} lg={2}>
+        <Feature image={PaymentIcon} title="Ödemeler" />
+      </Grid>
+
+      <Grid item xs={6} sm={4} md={3} lg={2}>
+        <Feature image={BarChartIcon} title="Finansal" />
       </Grid>
     </Grid>
   );
