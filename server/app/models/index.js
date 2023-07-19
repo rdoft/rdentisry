5d6af67dd94fa9f6ec56c56ef29122e0d66bf092
recepdemrci <recepdemiric.14@gmail.com>
2023-07-20 00:08:44 +0300

    RDEN-114 🐞 Fix in tedious package
    try to fix tedious error

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,10 +1,11 @@
+import * as tedious from 'tedious';
 const config = require("../config/db.config");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   host: config.HOST,
   dialect: config.dialect,
-
+  dialectModule: tedious,
   pool: {
     max: config.pool.max,
     min: config.pool.min,
