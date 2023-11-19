32a0534f5767a717f844915ce6158e1b8c2e8360
recepdemrci <recepdemiric.14@gmail.com>
2023-11-20 00:49:33 +0300

    RDEN-0 🚧 Refactor in app name
    Updated app name from esterno to dendis

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -19,7 +19,7 @@ const Logo = ({ sx, to }) => (
   >
     <Stack direction="row" spacing={2} alignItems="center">
       <LogoImage />
-      <Typography variant="h4">esterno</Typography>
+      <Typography variant="h4">dendis</Typography>
     </Stack>
   </ButtonBase>
 );
