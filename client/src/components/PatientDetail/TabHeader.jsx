e70d025949dbf7f97a5c4c37d215c3dc3e89c7f2
recepdemrci <recepdemiric.14@gmail.com>
2023-12-05 00:08:14 +0300

    RDEN-157 🐞 Fix in tabs
    Fixed the color of tabs on PatientDetails page

--- client/src/components/PatientDetail/TabHeader.jsx
+++ client/src/components/PatientDetail/TabHeader.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Badge } from "primereact";
 
-function TabHeader({ label, badge, onClick }) {
+function TabHeader({ label, badge, onClick, isActive }) {
   return (
     <div
       className="flex align-items-center px-3"
       style={{ cursor: "pointer" }}
       onClick={onClick}
     >
-      <h4>{label}</h4>
+      <h4 style={{ color: isActive ? 'primary' : '' }}>{label}</h4>
       <Badge severity="secondary" value={badge ? badge : 0} className="ml-2"></Badge>
     </div>
   );
