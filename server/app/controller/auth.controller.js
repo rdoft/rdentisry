f499d76a2c43205c3047a80eb743abe0db7aa0f0
recepdemrci <recepdemiric.14@gmail.com>
2024-01-02 19:21:25 +0300

    RDEN-235 🐞 Fix in register
    Fixed register with same email

--- server/app/controller/auth.controller.js
+++ server/app/controller/auth.controller.js
@@ -21,33 +21,37 @@ exports.register = async (req, res) => {
   const { name, email, password } = req.body;
   let user;
 
-  // Generate a salt
-  const salt = await bcrypt.genSalt(10);
-  // Hash the password with the salt
-  const hashedPassword = await bcrypt.hash(password, salt);
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password with the salt
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  // Add user record
-  user = await User.findOne({
-    where: {
-      Email: email,
-    },
-  });
-
-  if (user) {
-    return res.status(400).send({ message: "Mail adresi zaten kayıtlı" });
-  }
-
-  user = await User.create({
-    Name: name,
-    Email: email,
-    Password: hashedPassword,
-  });
-
-  // Login user
-  req.login(user, function (err) {
-    if (err) {
-      return next(err);
+    // Control if user exists
+    user = await User.findOne({
+      where: {
+        Email: email,
+      },
+    });
+    if (user) {
+      return res.status(400).send({ message: "Mail adresi zaten kayıtlıdır" });
     }
-    res.status(200).send();
-  });
+
+    // Create user record
+    user = await User.create({
+      Name: name,
+      Email: email,
+      Password: hashedPassword,
+    });
+
+    // Login user
+    req.login(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).send();
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
