4a07feca931a6b9742ccfa339371b81c4c0172e4
recepdemrci <recepdemiric.14@gmail.com>
2024-02-20 11:07:00 +0300

    RDEN-309 🚧 Refactor in dialog footer
    Updated <></>
    Updated buttons to be smaller in size

--- client/src/components/DialogFooter/DialogFooter.jsx
+++ client/src/components/DialogFooter/DialogFooter.jsx
@@ -3,26 +3,33 @@ import { Button } from "primereact";
 
 function DialogFooter({ disabled, onHide, onSubmit, onDelete }) {
   return (
-    <React.Fragment>
+    <>
       {onSubmit && (
         <Button
           disabled={disabled}
           label="Kaydet"
+          size="small"
           className="p-button p-button-info"
           onClick={onSubmit}
         />
       )}
       {onDelete && (
-        <Button label="Sil" className="p-button-danger" onClick={onDelete} />
+        <Button
+          label="Sil"
+          size="small"
+          className="p-button-danger"
+          onClick={onDelete}
+        />
       )}
       {onHide && (
         <Button
           label="İptal"
+          size="small"
           className="p-button-text p-button-secondary"
           onClick={onHide}
         />
       )}
-    </React.Fragment>
+    </>
   );
 }
 
