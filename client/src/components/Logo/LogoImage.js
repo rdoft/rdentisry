54d23d0495f47e43dba1f810509e467988f77295
recepdemrci <recepdemiric.14@gmail.com>
2024-02-20 15:14:53 +0300

    RDEN-311 🚧 Refactor in Logo
    Refactor small things

--- client/src/components/Logo/LogoImage.js
+++ client/src/components/Logo/LogoImage.js
@@ -1,12 +1,7 @@
 import { ReactComponent as Logo } from "assets/svg/dishekime/dishekime.svg";
 
-// ==============================|| LOGO SVG ||============================== //
-
 const LogoImage = () => {
-  return (
-    // <img src={logo} alt="Mantis" width="45" />
-    <Logo style={{ width: "70%" }} />
-  );
+  return <Logo style={{ width: "70%" }} />;
 };
 
 export default LogoImage;
