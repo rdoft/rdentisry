411d8004fd760a4d73022c24b85e5e23f3e90b5b
recepdemrci <recepdemiric.14@gmail.com>
2024-06-09 14:37:23 +0300

    RDEN-386 🚧 Refactor on payments tab
    Updated statistic card text and added total
    Updated progressbar

--- client/src/components/PatientDetail/Payments/PaymentStatistic.jsx
+++ client/src/components/PatientDetail/Payments/PaymentStatistic.jsx
@@ -3,13 +3,22 @@ import { Grid } from "@mui/material";
 import { StatisticCard } from "components/cards";
 
 export function PaymentStatistic({
+  totalAmount,
   completedAmount,
   waitingAmount,
   overdueAmount,
 }) {
   return (
     <Grid container item xs={12} p={2} spacing={3} justifyContent="center">
-      <Grid item xs={2}>
+      <Grid item xs={3} md={2}>
+        <StatisticCard
+          label={"Tedavi Tutarı"}
+          amount={totalAmount}
+          backgroundColor="#F5F5F5"
+          color="#182A4C"
+        ></StatisticCard>
+      </Grid>
+      <Grid item xs={3} md={2}>
         <StatisticCard
           label={"Ödenen"}
           amount={completedAmount}
@@ -17,7 +26,7 @@ export function PaymentStatistic({
           color="#22A069"
         ></StatisticCard>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3} md={2}>
         <StatisticCard
           label={"Kalan"}
           amount={waitingAmount}
@@ -26,14 +35,14 @@ export function PaymentStatistic({
         ></StatisticCard>
       </Grid>
       {/* {overdueAmount !== 0 && ( */}
-        <Grid item xs={2}>
-          <StatisticCard
-            label={"Vadesi Geçen"}
-            amount={overdueAmount}
-            backgroundColor="#FFD2CB"
-            color="#EF4444"
-          ></StatisticCard>
-        </Grid>
+      <Grid item xs={3} md={2}>
+        <StatisticCard
+          label={"Vadesi Geçen"}
+          amount={overdueAmount}
+          backgroundColor="#FFD2CB"
+          color="#EF4444"
+        ></StatisticCard>
+      </Grid>
       {/* )} */}
     </Grid>
   );
