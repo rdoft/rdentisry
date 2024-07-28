fb2236228853a98b0ee45e99ad3271706cddf3ba
recepdemrci <recepdemiric.14@gmail.com>
2024-07-29 00:24:36 +0300

    RDEN-444 🎮 Feat in loading
    Added loading spinner component

---
+++ client/src/components/Other/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// assets
+import "assets/styles/Other/Loading.css";
+
+const LoadingSpinner = () => (
+  <div className="spinner-container">
+    <div className="loading-spinner"></div>
+  </div>
+);
+
+export default LoadingSpinner;
